a41bc4df07e75bf3b22954546b99d4e130b68a7b
hellun205 <hellun515@gmail.com>
2023-08-17 11:28:22 +0900

    player character model and movement

---
+++ Assets/Scripts/Manager/Managers.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Manager
+{
+  public class Managers : MonoBehaviour
+  {
+    private static Managers s_Instance;
+
+    public static Managers Instance
+    {
+      get
+      {
+        Init();
+        return s_Instance;
+      }
+    }
+
+    public void Start()
+    {
+      Init();
+    }
+
+    private static void Init()
+    {
+      if (s_Instance is not null) return;
+    
+      var go = GameObject.Find("@Managers");
+      if (go is  null)
+      {
+        go = new GameObject { name = "@Managers" };
+        go.AddComponent<Managers>();
+      }
+
+      DontDestroyOnLoad(go);
+      s_Instance = go.GetComponent<Managers>();
+    }
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,16 @@
 using System;
+using Manager;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  private void Start()
+  public float moveSpeed = 1f;
+
+  private void Update()
   {
-    var mg = Managers.Instance;
+    var h = Input.GetAxis("Horizontal");
+    var v = Input.GetAxis("Vertical");
+
+    transform.Translate(new Vector3(h , 0, v )  * Time.deltaTime * moveSpeed);
   }
 }
